f22459eec9dff465f07f1513ea00c1e8f0bcf067
DinoBudino <zahir_mauricio@outlook.es>
2024-05-18 02:37:00 -0500

    estructura generada

---
+++ src/main/java/com/makethedifference/mtd/MtdApplication.java
@@ -0,0 +1,13 @@
+package com.makethedifference.mtd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makethedifference/mtd/MtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makethedifference.mtd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
