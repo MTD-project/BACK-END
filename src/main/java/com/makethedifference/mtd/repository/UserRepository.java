0034736d2e7edbaeabcd0c8370321ff6c09068ef
DinoBudino <zahir_mauricio@outlook.es>
2024-05-18 03:08:51 -0500

    Controladores para usuario, repositorio y service creados

---
+++ src/main/java/com/makethedifference/mtd/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.makethedifference.mtd.repository;
+
+import com.makethedifference.mtd.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
